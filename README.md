7003558a2fc41505a528dfd68b61a47ace769b4a
tknr <geishagirlskoume@gmail.com>
2021-12-25 16:30:31 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # shell_docker_osx
+
+## ref
+
+https://github.com/sickcodes/Docker-OSX#initial-setup
+
---
+++ monterey.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run -it \
+    --device /dev/kvm \
+    -p 50922:10022 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e "DISPLAY=${DISPLAY:-:0.0}" \
+    -e GENERATE_UNIQUE=true \
+    -e MASTER_PLIST_URL='https://raw.githubusercontent.com/sickcodes/osx-serial-generator/master/config-custom.plist' \
+    sickcodes/docker-osx:monterey
+
+# docker build -t docker-osx --build-arg SHORTNAME=monterey .
+
